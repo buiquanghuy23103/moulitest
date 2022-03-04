678b2749580e245f9cbb036c7d79eb8fa3182bb2
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-03-04 21:59:01 +0200

    add zero flag test for f conv

--- ft_printf_tests/tests/93_conv_f.spec.c
+++ ft_printf_tests/tests/93_conv_f.spec.c
@@ -26,10 +26,16 @@ static void simple_negative_large_nbr(t_test *test)
 	assert_printf("{%f}{%lf}{%Lf}", -1444565444646.6465424242242454654, -1444565444646.6465424242242454654, -1444565444646.6465424242242454654l);
 }
 
+static void zero_flag(t_test *test)
+{
+	assert_printf("%012f - %012lf - %012Lf\n", 1.42, 1.42, 1.42l);
+}
+
 void	suite_93_conv_f(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, simple_small_nbr);
 	SUITE_ADD_TEST(suite, simple_negative_small_nbr);
 	SUITE_ADD_TEST(suite, simple_large_nbr);
 	SUITE_ADD_TEST(suite, simple_negative_large_nbr);
+	SUITE_ADD_TEST(suite, zero_flag);
 }
