bfa90fbd89051914d4d876210b15841187bfd7db
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-03-04 22:26:35 +0200

    add space flag test for f conv

--- ft_printf_tests/tests/93_conv_f.spec.c
+++ ft_printf_tests/tests/93_conv_f.spec.c
@@ -33,7 +33,12 @@ static void zero_flag(t_test *test)
 
 static void plus_flag(t_test *test)
 {
-	assert_printf("C: %+06.0f - %+06.0lf - %+06.0Lf\n", 1.42, 1.42, 1.42l);
+	assert_printf("%+06.0f - %+06.0lf - %+06.0Lf\n", 1.42, 1.42, 1.42l);
+}
+
+static void space_flag(t_test *test)
+{
+	assert_printf("% 06.0f - % 06.0lf - % 06.0Lf\n", 1.42, 1.42, 1.42l);
 }
 
 void	suite_93_conv_f(t_suite *suite)
@@ -44,4 +49,5 @@ void	suite_93_conv_f(t_suite *suite)
 	SUITE_ADD_TEST(suite, simple_negative_large_nbr);
 	SUITE_ADD_TEST(suite, zero_flag);
 	SUITE_ADD_TEST(suite, plus_flag);
+	SUITE_ADD_TEST(suite, space_flag);
 }
